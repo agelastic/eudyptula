702234cf0c6bcb39d74eb4bfe47e12ca3a799e36
Vitaly Osipov <vitaly.osipov@gmail.com>
2014-04-21 22:38:17 +1000

    use proper syscall macro

--- task15/test15.c
+++ task15/test15.c
@@ -8,14 +8,8 @@ int main(int argc, char *argv[])
 	int result;
 
 	printf("1. Calling sys_eudyptula with params (1,1)\n");
-	if (syscall(EUDY,1,1))
-		printf("Test OK\n");
-	else
-		printf("Test failed\n");
-
+	printf("Syscal returns %i\n", syscall(EUDY,1,1));
 	printf("2. Calling sys_eudyptula with params (0x7c1c,0xaf2f50d1)\n");
-	if (!syscall(EUDY,0x7c1c,0xaf2f50d1))
-		printf("Test OK\n");
-	else
-		printf("Test failed\n");
+	printf("Syscal returns %i\n", syscall(EUDY,0x7c1c,0xaf2f50d1));
+
 }
